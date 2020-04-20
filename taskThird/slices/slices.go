5c83ff94800521150aa08dc68f8f3b73fafcd5b9
unknown <vita.zhdanko@mail.ru>
2020-04-20 20:39:29 +0300

    updated task

--- taskThird/slices/slices.go
+++ taskThird/slices/slices.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 )
 
@@ -8,46 +9,59 @@ func main() {
 	sliceString1 := []string{"one", "two", "three"}
 	sliceString2 := []string{"one", "two"}
 	sliceString3 := []string{}
-	fmt.Println("SliceString1: ", sliceString1)
-	fmt.Println("The longest word from the sliceString1: ", max(sliceString1))
-	fmt.Println("SliceString2: ", sliceString2)
-	fmt.Println("The longest word from the sliceString2: ", max(sliceString2))
-	fmt.Println("SliceString3: ", sliceString3)
-	fmt.Println("The longest word from the sliceString3: ", max(sliceString3))
+
+	printStringSlice(sliceString1)
+	printStringSlice(sliceString2)
+	printStringSlice(sliceString3)
 
 	sliceInt1 := []int64{1, 2, 5, 15}
-	fmt.Println("SliceInt: ", sliceInt1)
-	fmt.Println("Reverse sliceInt: ", reverse(sliceInt1))
-	fmt.Println("SliceInt: ", sliceInt1)
 	sliceInt2 := []int64{}
-	fmt.Println("SliceInt: ", sliceInt2)
-	fmt.Println("Reverse sliceInt: ", reverse(sliceInt2))
+
+	printIntSlice(sliceInt1)
+	printIntSlice(sliceInt2)
 }
 
-func max(slice []string) string {
-	var maxWord string
+func printStringSlice(slice []string) {
+	fmt.Println("SliceString: ", slice)
+	max, err := max(slice)
+	if err != nil {
+		fmt.Println("Error...", err)
+		return
+	}
+	fmt.Println("The longest word from the sliceString: ", max)
+}
+
+func printIntSlice(slice []int64) {
+	fmt.Println("SliceInt: ", slice)
+	reverse, err := reverse(slice)
+	if err != nil {
+		fmt.Println("Error...", err)
+		return
+	}
+	fmt.Println("Reverse sliceInt: ", reverse)
+	fmt.Println("SliceInt: ", slice)
+}
+
+func max(slice []string) (string, error) {
 	if len(slice) == 0 {
-		maxWord = "Slice is empty!"
-	} else {
-		maxWord = slice[0]
-		for _, value := range slice {
-			if len(maxWord) < len(value) {
-				maxWord = value
-			}
+		return "", errors.New("Slice is empty")
+	}
+	var maxWord string = slice[0]
+	for _, value := range slice {
+		if len(maxWord) < len(value) {
+			maxWord = value
 		}
 	}
-	return maxWord
+	return maxWord, nil
 }
 
-func reverse(slice []int64) []int64 {
+func reverse(slice []int64) ([]int64, error) {
 	var reverseSlice []int64
 	if len(slice) == 0 {
-		fmt.Println("Slice is empty")
-		reverseSlice = nil
-	} else {
-		for i := len(slice) - 1; i >= 0; i-- {
-			reverseSlice = append(reverseSlice, slice[i])
-		}
+		return nil, errors.New("Slice is empty")
 	}
-	return reverseSlice
+	for i := len(slice) - 1; i >= 0; i-- {
+		reverseSlice = append(reverseSlice, slice[i])
+	}
+	return reverseSlice, nil
 }
