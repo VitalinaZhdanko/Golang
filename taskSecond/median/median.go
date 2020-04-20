3dd63c2d456195424f12f77bb54d2d39d25de80c
unknown <vita.zhdanko@mail.ru>
2020-04-20 18:54:58 +0300

    updated task

--- taskSecond/median/median.go
+++ taskSecond/median/median.go
@@ -14,25 +14,24 @@ func main() {
 
 	if number == 0 {
 		fmt.Println("Enter correct data")
-	} else {
-		slice := make([]int, number)
-		for i := 0; i < number; i++ {
-			slice[i] = rand.Intn(100)
-		}
-		fmt.Println(slice)
-		fmt.Println(median(slice))
+		return
 	}
+	slice := make([]int, number)
+	for i := 0; i < number; i++ {
+		slice[i] = rand.Intn(100)
+	}
+	fmt.Println(slice)
+	fmt.Println(median(slice))
+
 }
 
 func median(slice []int) float64 {
 	sort.Ints(slice)
-	var num float64
 	if len(slice)%2 == 0 {
 		fmt.Println("Sorted even array: ", slice)
-		num = float64((slice[len(slice)/2] + slice[len(slice)/2-1])) / 2
-	} else {
-		fmt.Println("Sorted odd array: ", slice)
-		num = float64(slice[len(slice)/2])
+		return float64((slice[len(slice)/2] + slice[len(slice)/2-1])) / 2
 	}
-	return num
+	fmt.Println("Sorted odd array: ", slice)
+	return float64(slice[len(slice)/2])
+
 }
